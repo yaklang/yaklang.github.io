2f896b3ded1bd53ec2eae2916d4c53db745e62ff
v1ll4n <v1ll4n@qq.com>
2021-06-18 17:31:18 +0800

    add hotjar

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,6 +39,17 @@ export default function Home() {
         <Layout
             title={`Yak Language ${siteConfig.title}`}
             description="立志成为Web安全能力研发最强语言">
+            <!-- Hotjar Tracking Code for yaklang.github.io -->
+            <script>
+                (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2460188,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            </script>
             <HomepageHeader/>
             <main>
                 <HomepageFeatures/>
