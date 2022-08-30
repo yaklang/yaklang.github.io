c8eea387426080482d6eba68e163034f45f1b699
nonight <378099757@qq.com>
2022-08-30 14:24:04 +0800

    feat: 企业页面开发

--- src/components/EnterpriseCollaboration.tsx
+++ src/components/EnterpriseCollaboration.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { Popover } from "antd";
 import { PhoneSvgIcon, WechatSvgIcon } from "./icons";
 import { QrcodeOutlined, DownloadOutlined } from "@ant-design/icons";
-import { SelectedSvgIcon } from "./cooperateIcon";
+import { CriticalBugSvgIcon, HighBugSvgIcon, LowBugSvgIcon, SelectedSvgIcon } from "./cooperateIcon";
 
 export interface EnterpriseCollaborationPageProps {}
 
@@ -249,26 +249,26 @@ export const EnterpriseCollaborationPage: React.FC<EnterpriseCollaborationPagePr
                 </div>
             </div>
 
-            <div className="cooperation-wrapper">
+            <div className="cooperation-wrapper"> 
                 <div className="cooperation-body">
                     <div className="cooperation-title">
                         企业版合作<span className="theme-style">专享</span>
                     </div>
 
-                    <div className="cooperation-module-a">
+                    <div className="cooperation-module-a cooperation-module-layout">
                         <div className="module-a-img">
                             <img
                                 src={require("../../static/img/cooperation/cooperation-1.png").default}
                                 className="img-style"
                             />
                         </div>
-                        <div className="module-a-content">
+                        <div className="module-a-content module-content-style">
                             <div className="content-title">实现企业安全工具的统一管理</div>
-                            <div className="content-style">
+                            <div className="a-style content-style">
                                 构建企业渗透测试全流程工具并进行统一管理，工具原生协同，迅速拉平企业内部安全人员的
                                 “硬件” 水平。
                             </div>
-                            <div className="content-topic topic-first">全流程工具包括</div>
+                            <div className="content-topic a-topic-first">全流程工具包括</div>
                             <div className="content-topic-title">
                                 <div className="title-opt">
                                     <SelectedSvgIcon className="icon-style" />
@@ -283,7 +283,7 @@ export const EnterpriseCollaborationPage: React.FC<EnterpriseCollaborationPagePr
                                     <div className="title-style">提供工具定制开发</div>
                                 </div>
                             </div>
-                            <div className="content-topic topic-second">专属插件仓库</div>
+                            <div className="content-topic a-topic-second">专属插件仓库</div>
                             <div className="content-topic-title">
                                 <div className="title-opt">
                                     <SelectedSvgIcon className="icon-style" />
@@ -297,13 +297,131 @@ export const EnterpriseCollaborationPage: React.FC<EnterpriseCollaborationPagePr
                         </div>
                     </div>
 
-                    <div className="cooperation-module-b"></div>
+                    <div className="cooperation-module-b cooperation-module-layout">
+                        <div className="module-b-content module-content-style">
+                            <div className="content-title">企业安全团队管理与任务协作</div>
+                            <div className="b-style content-style">
+                                对安全团队成员进行管理，实现团队紧密协作，高效工作。
+                            </div>
+                            <div className="content-topic b-topic-first">成员管理</div>
+                            <div className="content-topic-title b-topic-title">
+                                基于认证的访问控制,基于角色的授权管理
+                            </div>
+                            <div className="content-topic b-topic-second">任务协同</div>
+                            <div className="content-topic-title b-topic-title">
+                                可以组建任务组,实现安全测试任务的协作或者接力执行
+                            </div>
+                            <div className="content-topic b-topic-third">安全数据共享</div>
+                            <div className="content-topic-title b-topic-title">
+                                参与任务的成员可以查看所有任务相关数据
+                            </div>
+                        </div>
 
-                    <div className="cooperation-module-c"></div>
+                        <div className="module-b-img">
+                            <img
+                                src={require("../../static/img/cooperation/cooperation-2.png").default}
+                                className="img-style"
+                            />
+                        </div>
+                    </div>
 
-                    <div className="cooperation-module-d"></div>
+                    <div className="cooperation-module-c cooperation-module-layout">
+                        <div className="module-c-img">
+                            <img
+                                src={require("../../static/img/cooperation/cooperation-3.png").default}
+                                className="img-style"
+                            />
+                        </div>
 
-                    <div className="cooperation-module-e"></div>
+                        <div className="module-c-content module-content-style">
+                            <div className="content-title">针对新漏洞的快速响应</div>
+                            <div className="c-style content-style">
+                                针对网络空间新发现的漏洞，快速提供针对性的检测插件，按照影响程度进行分级响应。
+                            </div>
+                            <div className="bug-title bug-first">
+                                <LowBugSvgIcon className="icon-style" />
+                                <div className="title-style">
+                                    一般漏洞 - <span className="theme-style">48</span> 小时内
+                                </div>
+                            </div>
+                            <div className="bug-title bug-second">
+                                <HighBugSvgIcon className="icon-style" />
+                                <div className="title-style">
+                                    大型影响漏洞 - <span className="theme-style">24</span> 小时内
+                                </div>
+                            </div>
+                            <div className="bug-title bug-third">
+                                <CriticalBugSvgIcon className="icon-style" />
+                                <div className="title-style">
+                                    重大影响漏洞 - <span className="theme-style">12</span> 小时内
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="cooperation-module-d cooperation-module-layout">
+                        <div className="module-d-content module-content-style">
+                            <div className="content-title">Yakit 产品部署方案</div>
+                            <div className="d-style content-style">
+                                基于 Yaklang 的低代码平台，融合“图灵完备”低代码 +
+                                安全体系，针对企业部署需求提供不同的产品部署解决方案，极致降低安全运营成本。
+                            </div>
+                            <div className="content-topic d-topic-first">分布式引擎 SaaS 化部署</div>
+                            <div className="content-topic-title d-topic-title-first">
+                                ▫ 针对公网业务系统可以直接注册账号使用，用户无需购买，安装或维护任何软、硬件
+                            </div>
+                            <div className="content-topic-title d-topic-title-second">
+                                ▫ 分布式调度 Yaklang 引擎，快速实现组织内任务的分发和管理
+                            </div>
+                            <div className="content-topic d-topic-second">私有化部署</div>
+                            <div className="content-topic-title d-topic-title-first">
+                                ▫ 针对内网业务系统可以在内网直接部署 Yakit 服务器，配置安装后即可上线使用
+                            </div>
+                            <div className="content-topic-title d-topic-title-second">
+                                ▫ 持续“快速灵活低成本”挂载更多低代码服务和应用
+                            </div>
+                        </div>
+
+                        <div className="module-d-img">
+                            <img
+                                src={require("../../static/img/cooperation/cooperation-4.png").default}
+                                className="img-style"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="cooperation-module-e cooperation-module-layout">
+                        <div className="module-e-img">
+                            <img
+                                src={require("../../static/img/cooperation/cooperation-5.png").default}
+                                className="img-style"
+                            />
+                        </div>
+
+                        <div className="module-e-content module-content-style">
+                            <div className="content-title">企业专属定制化服务和合作</div>
+                            <div className="e-style content-style">
+                                针对企业级用户的个性化定制需求，打造贴合企业业务真实场景的服务方案。
+                            </div>
+                            <div className="content-topic e-topic-first">定制化服务</div>
+                            <div className="content-topic-title e-topic-title">
+                                如果您的安全团队在日常检测工作中有其他难题或者需求，我们可以提供定制服务
+                            </div>
+                            <div className="content-topic e-topic-second">企业合作</div>
+                            <div className="content-topic-title e-topic-title">
+                                如果您的团队有基于 Yak 的课题工程转化或者科研课题探讨的想法，欢迎
+                                <span
+                                    className="theme-style"
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        window.scrollTo(0, document.body.scrollHeight);
+                                    }}
+                                >
+                                    联系我们
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
